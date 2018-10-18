a25886d6a12bb00be1fa62896b539932a3b9ca76
princefair1 <42935207+princefair1@users.noreply.github.com>
2018-10-18 18:31:20 +0200

    Update tgroupa.js

--- tgroupa.js
+++ tgroupa.js
@@ -29,9 +29,6 @@ function timerFunc() {
 var timer = setTimeout(timerFunc, 1000);
 client1.on("ready", () => {
 
-	      var interval = setInterval (function () {
-client1.channels.get('502231555163553792').send('لا إله الا الله, سبحان الله, الحمدالله, الله اكبر');		  }, 100000);
-  
 	      var interval1 = setInterval (function () {
   client1.channels.get('499967544912773120').send('#daily');
   client1.channels.get('499967544912773120').send('#rep <@401053902168129556>');  
@@ -46,20 +43,7 @@ client1.channels.get('499967544912773120').send('t!rep <@401053902168129556>');
 client1.login(process.env.BOT_TOKEN); 
 
 
-const client1 = new Discord.client1();
-
-
-client1.on('warn', console.warn);
-
-client1.on('error', console.error);
-
-
-
-client1.on('ready', () => console.log('ProBot Credits Miner Discord.js Script'));
-
-client1.on('disconnect', () => console.log('PROBOT credits miner had disconnected!'));
-
-client1.on('reconnecting', () => console.log('PROBOT credits miner is reconnecting...'));
+const client1 = new Discord.client();
 
 
 function timerFunc() {
@@ -73,9 +57,7 @@ function timerFunc() {
 var timer = setTimeout(timerFunc, 1000);
 client1.on("ready", () => {
 
-	      var interval = setInterval (function () {
-client1.channels.get('502231555163553792').send('لا إله الا الله, سبحان الله, الحمدالله, الله اكبر');		  }, 100000);
-  
+	     
 	      var interval1 = setInterval (function () {
   client1.channels.get('499967544912773120').send('#daily');
   client1.channels.get('499967544912773120').send('#rep <@401053902168129556>');  
